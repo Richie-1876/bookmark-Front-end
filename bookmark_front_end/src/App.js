1885838c3b311e97be02b66e0aed1709745180b9
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2020-04-03 19:02:47 +0100

    add bookmark button toggles and when the form submits they get added to state.

--- bookmark_front_end/src/App.js
+++ bookmark_front_end/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Bookmark from './components/Bookmark.js'
+import Newform from './components/Newform.js'
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
   super(props)
   this.state = {
-    bookmarks: []
+    bookmarks: [],
+    newBookmark: false
   }
+  this.handleAddBookmark = this.handleAddBookmark.bind(this)
+}
+handleAddBookmark(bookmark){
+  const copyBookmarks = [bookmark, ...this.state.bookmarks]
+  this.setState({
+    bookmarks: copyBookmarks
+  })
+}
+toggleNewForm(){
+  this.setState({
+    newBookmark: !this.state.newBookmark
+  })
 }
 fetchBookmarks = async () => {
     let response = await fetch('http://localhost:3000/bookmarks')
@@ -22,9 +36,17 @@ fetchBookmarks = async () => {
 render() {
   return (
     <div>
+    <button onClick={()=>{this.toggleNewForm()}}>Add new Bookmark</button>
+    {
+             this.state.newBookmark
+             ? <Newform toggleNewForm={this.toggleNewForm}  handleAddBookmark={this.handleAddBookmark}/>
+             : null
+    }
+    <div>
       {this.state.bookmarks.map(bookmark =>
         <Bookmark bookmark={bookmark}/>
       )}
+      </div>
     </div>
   )
 }
---
+++ bookmark_front_end/src/components/Newform.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+
+export default class Newform extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      logo: '',
+      description: '',
+      link: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+  handleChange(e){
+    this.setState({[e.target.id]: e.target.value})
+  }
+async  handleSubmit(e){
+    e.preventDefault()
+    try {
+      let response = await fetch(`http://localhost:3000/bookmarks`, {
+        method:'POST',
+        body: JSON.stringify({
+          name: this.state.name,
+          logo: this.state.logo,
+          description: this.state.description,
+          link: this.state.link
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      let data = await response.json()
+      this.prop.handleAddBookmark(data)
+      this.props.toggleNewForm()
+      this.state({
+        name: '',
+        logo: '',
+        description: '',
+        link: ''
+      })
+    } catch (e) {
+      console.error('Error',e)
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' id='name' placeholder='name'onChange={this.handleChange}/>
+
+        <input type='text' id='logo'placeholder='logo'onChange={this.handleChange}/>
+
+        <textarea rows='4' cols='50' id='description'placeholder='description'onChange={this.handleChange}></textarea>
+
+        <input type='text' id='link'placeholder='url'onChange={this.handleChange}/>
+
+        <input type="submit"/>
+
+
+      </form>
+      </>
+    )
+  }
+}
